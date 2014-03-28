5eecb808d996e881dab33ebbd16c56434494614e
Jon <jonathan.j.mason@gmail.com>
2014-03-28 14:47:31 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Conjur",
+    version = "0.1.0",
+    packages = find_packages(),
+    install_requires = ['mock', 'requests'],
+    package_data = {},
+    author = "Jon Mason",
+    author_email = "jon@conjur.net",
+    description = "Python client for the Conjur API",
+    license = "MIT"
+)
