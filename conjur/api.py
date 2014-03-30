5e69caf5659d59acc1511a8613cfb2844db9ec23
Jon <jonathan.j.mason@gmail.com>
2014-03-30 01:52:51 -0500

    remove some cruft and begin work on variables

--- conjur/api.py
+++ conjur/api.py
@@ -1,5 +1,9 @@
 import requests
+import base64
+from conjur.variable import Variable
+
 from conjur import ConjurException
+
 
 class API(object):
     def __init__(self, credentials=None, token=None, config=None):
@@ -17,9 +21,11 @@ class API(object):
             from conjur.config import config
             self.config = config
 
-
-
     def authenticate(self, cached=True):
+        """
+        Authenticate with Conjur and return a token (str) that can be used
+        to establish identity to Conjur services.
+        """
         if cached and self.token:
             return self.token
         if not self.login or not self.api_key:
@@ -27,5 +33,48 @@ class API(object):
         url = "%s/users/%s/authenticate"%(self.config.authn_url, self.login)
         response = requests.post(url, self.api_key)
         if response.status_code != 200:
-            raise Exception("Authentication failed: %d"%(response.status_code))
+            raise Exception("Authentication failed: %d"%(response.status_code,))
         return response.text
+
+    def auth_header(self):
+        """
+        Get the value of an Authorization header to make Conjur requests, performing
+        authentication if necessary.
+        """
+        token = self.authenticate()
+        enc = base64.b64encode(token)
+        return 'Token token="%s"'%enc
+
+
+    def request(self, method, url, **kwargs):
+        """
+        Make an authenticated request.  Additional arguments are passed
+        to requests.method.
+        """
+        headers = kwargs.setdefault('headers', {})
+        headers['Authorization'] = self.auth_header()
+        response = getattr(requests, method.lower())(url, **kwargs)
+        if response.status_code >= 300:
+            raise ConjurException("Request failed: %d"%(response.status_code))
+        return response
+
+    def get(self, url, **kwargs):
+        return self.request('get', url, **kwargs)
+
+    def post(self, url, **kwargs):
+        return self.request('post', url, **kwargs)
+
+    def put(self, url, **kwargs):
+        return self.request('put', url, **kwargs)
+
+    def variable(self, id):
+        return Variable(self, id)
+
+    def create_variable(self, id=None, mime_type='text/plain', kind='secret'):
+        data = { 'mime_type': mime_type, 'kind': kind }
+        if id:
+            data['id'] = id
+        attrs = self.post("%s/variables"%(self.config.core_url), data=data).json
+        id = id or attrs['id']
+        return Variable(self, id, attrs)
+
--- test/api_test.py
+++ test/api_test.py
@@ -1,6 +1,7 @@
 import conjur
 from mock import patch, Mock
 import requests
+import base64
 
 @patch.object(requests, 'post')
 def test_authenticate(mock_post):
@@ -18,3 +19,15 @@ def test_authenticate_with_cached_token(mock_post):
     api = conjur.new_from_token("token token")
     assert api.authenticate() == "token token"
     mock_post.assert_not_called()
+
+def test_auth_header():
+    api = conjur.new_from_token("the token")
+    expected = 'Token token="%s"'%(base64.b64encode("the token"))
+    assert api.auth_header() == expected
+
+@patch.object(requests, 'post')
+def test_request_with_post(mock_post):
+    api = conjur.new_from_token("the token")
+    mock_post.return_value = resp = Mock()
+    assert api.request('post', 'https://example.com', data="foobar") == resp
+    mock_post.assert_called_with('https://example.com', data="foobar", headers={"Authorization": api.auth_header()})
