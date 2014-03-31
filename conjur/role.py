b947465f4ba3aa280b73f1d5d989a8cab06d463e
Jon <jonathan.j.mason@gmail.com>
2014-03-31 17:44:23 -0500

    Add license headers

--- conjur/role.py
+++ conjur/role.py
@@ -18,24 +18,6 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import conjur
-from conjur.config import Config, config
-
-def test_new_from_key():
-    api = conjur.new_from_key("login", "secret")
-    assert api.token is None
-    assert api.api_key == "secret"
-    assert api.login == "login"
-    assert api.config == config
-
-def test_new_from_token():
-    api = conjur.new_from_token("a token")
-    assert api.token == "a token"
-    assert api.api_key == None
-    assert api.login == None
-    assert api.config == config
-
-def test_new_with_config():
-    cfg = Config()
-    api = conjur.new_from_key("login", "secret",cfg=cfg)
-    assert cfg == api.config
+class Role(object):
+    def __init__(self, api, roleid):
+        pass
--- tests/pubkeys_tests.py
+++ tests/pubkeys_tests.py
@@ -17,25 +17,3 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-import conjur
-from conjur.config import Config, config
-
-def test_new_from_key():
-    api = conjur.new_from_key("login", "secret")
-    assert api.token is None
-    assert api.api_key == "secret"
-    assert api.login == "login"
-    assert api.config == config
-
-def test_new_from_token():
-    api = conjur.new_from_token("a token")
-    assert api.token == "a token"
-    assert api.api_key == None
-    assert api.login == None
-    assert api.config == config
-
-def test_new_with_config():
-    cfg = Config()
-    api = conjur.new_from_key("login", "secret",cfg=cfg)
-    assert cfg == api.config
