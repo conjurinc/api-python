de6e2d5976bb4fb87ef562d0a143cf72bd944893
Jon <jonathan.j.mason@gmail.com>
2014-04-01 11:53:12 -0500

    test groups

--- tests/group_test.py
+++ tests/group_test.py
@@ -17,3 +17,32 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from mock import patch, Mock
+import conjur
+
+api = conjur.new_from_key('foo', 'bar')
+
+group = api.group('v1/admins')
+
+def test_group():
+    assert group.role.kind == 'group'
+    assert group.role.identifier == 'v1/admins'
+    assert group.role.roleid == api.config.account + ':group:v1/admins'
+
+@patch.object(group.role, 'grant_to')
+def test_add_member(mock_grant_to):
+    member = api.user('foo')
+    group.add_member(member)
+    mock_grant_to.assert_called_with(member, False)
+
+@patch.object(group.role, 'grant_to')
+def test_add_member_admin(mock_grant_to):
+    member = api.role('something', 'else')
+    group.add_member(member, True)
+    mock_grant_to.assert_called_with(member, True)
+
+@patch.object(group.role, 'revoke_from')
+def test_remove_member(mock_revoke_from):
+    member = api.user('foo')
+    group.remove_member(member)
+    mock_revoke_from.assert_called_with(member)
