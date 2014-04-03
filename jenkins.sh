a3fc334a09e66e38a4b3f659462cac245d23505b
Hleb Rubanau <g.rubanau@gmail.com>
2014-04-04 00:52:43 +0300

    tests should be called last in jenkins.sh ; generate documentation during the build

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,13 @@
 PYENV_HOME=$WORKSPACE/.pyenv/
-
+DOCDIR=$WORKSPACE/pydocs
 # Delete previously built virtualenv
 if [ -d $PYENV_HOME ]; then
     rm -rf $PYENV_HOME
 fi
+if [ -d $DOCDIR ]; then
+    rm -rf $DOCDIR
+fi
+mkdir -p $DOCDIR
 
 # Create virtualenv and install necessary packages
 pip install virtualenv
@@ -12,5 +16,12 @@ virtualenv --no-site-packages $PYENV_HOME
 pip install --quiet nosexcover
 pip install --quiet pylint
 pip install --quiet $WORKSPACE/
-nosetests --with-xcoverage --with-xunit --cover-package=conjur --cover-erase
+for file in conjur/*.py ; do 
+    module=` echo "$file" | sed -e 's/\//./' -e 's/.py//g' -e 's/\.__init__//' ` ;
+    pydoc -w $module ;  
+    mv "$module".html $DOCDIR
+done
+cp -v $DOCDIR/conjur.html $DOCDIR/index.html
+
 pylint -f parseable conjur/ | tee pylint.out
+nosetests --with-xcoverage --with-xunit --cover-package=conjur --cover-erase
