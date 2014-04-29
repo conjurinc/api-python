ecf067aca5f28281d041a97b20f68fd5dbe9652a
Kevin Gilpin <kgilpin@gmail.com>
2014-04-29 13:48:06 -0400

    Specify minimum requests version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Conjur",
-    version = "0.2.1",
+    version = "0.2.2",
     packages = find_packages(),
-    install_requires = ['mock', 'requests', 'pyyaml', 'behave'],
+    install_requires = ['mock', 'requests >= 2.2.1', 'pyyaml', 'behave'],
     package_data = {},
     author = "Jon Mason",
     author_email = "jon@conjur.net",
