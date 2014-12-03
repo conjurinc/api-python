8e54c26db10cc5dd3d23897b4a4dcb130f05943f
Jon Mason <jonathan.j.mason@gmail.com>
2014-12-03 15:48:14 -0600

    use appliance url from env var and pepify a few things

--- conjur/config.py
+++ conjur/config.py
@@ -94,13 +94,20 @@ class Config:
 
     authn_url = _service_url('authn', doc='URL for the authn service')
     core_url = _service_url('core', doc='URL for the core service')
-    authz_url = _service_url('authz', per_account=False, doc='URL for the authz service')
+    authz_url = _service_url('authz', per_account=False,
+                             doc='URL for the authz service')
     pubkeys_url = _service_url('pubkeys', doc='URL for the pubkeys service')
 
-    verify_ssl = _setting('verify_ssl', True, "Allows SSL verification to be disabled (development only!)")
-    cert_file = _setting('cert_file', None, "Path to certificate to verify ssl requests to appliance")
-    stack = _setting('stack', 'v4', 'Identifier for shared conjur services (hosted only)')
-    account = _setting('account', 'conjur', 'Conjur account identifier')
+    verify_ssl = _setting('verify_ssl', True,
+                "Allows SSL verification to be disabled (development only!)")
+
+    cert_file = _setting('cert_file', None,
+        "Path to certificate to verify ssl requests to appliance")
+
+    stack = _setting('stack', 'v4',
+                     'Identifier for shared conjur services (hosted only)')
+    account = _setting('account', 'conjur',
+                       'Conjur account identifier')
     appliance_url = _setting('appliance_url', None, 'URL for Conjur appliance')
 
 
--- features/environment.py
+++ features/environment.py
@@ -2,6 +2,8 @@ import conjur
 from conjur.config import config
 import os
 import random
+
+
 def admin_password():
     if 'CONJUR_ADMIN_PASSWORD' not in os.environ:
         if 'CONJUR_ADMIN_PASSWORD_FILE' in os.environ:
@@ -9,12 +11,22 @@ def admin_password():
                 os.environ['CONJUR_ADMIN_PASSWORD'] = f.read()
     return os.environ['CONJUR_ADMIN_PASSWORD']
 
+
+def appliance_url():
+    return os.environ.get('CONJUR_APPLIANCE_URL', None)
+
+
 def api():
     config.stack = config.account = 'ci'
+    url = appliance_url()
+    if url is not None:
+        config.appliance_url = url
     return conjur.new_from_key('admin', admin_password())
 
+
 def random_string(prefix, size=8):
-    return "%s-%030x"%(prefix, random.randrange(16 ** size))
+    return "%s-%030x" % (prefix, random.randrange(16 ** size))
+
 
 def before_all(context):
     context.api = api()
