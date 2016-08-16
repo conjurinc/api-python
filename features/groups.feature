7bd920976d7dbd12e7bbe4558cac7b8d9208bbf3
Rafał Rzepecki <divided.mind@gmail.com>
2016-08-16 18:41:00 +0200

    Update group features

--- features/groups.feature
+++ features/groups.feature
@@ -1,23 +1,5 @@
 Feature: Group Management
 
-  Scenario: I can create groups
-    When I try to create a group "foo"
-    Then it succeeds
-
   Scenario: I can list group memberships
-    When I create a group "bar"
-    Then I can list the group members
-
-  Scenario: I can add members to a group
-    When I create a group "developers"
-    And I create a user "bob"
-    And I add the user to the group
-    Then the user is a member of the group
-
-  Scenario: I can remove members from a group
-    When I create a group "test"
-    And I create a user "bill"
-    And I add the user to the group
-    Then the user is a member of the group
-    When I remove the user from the group
-    Then the user is not a member of the group
+    When I list members of "group:everyone"
+    Then "cucumber:user:alice" is a member
--- features/steps/group_steps.py
+++ features/steps/group_steps.py
@@ -1,64 +1,12 @@
 from behave import when, then
 
 
-def group_has_member(group, user):
-    members = group.members()
-    userid = user.role.roleid
-    for m in members:
-        if m['member'] == userid:
-            return True
-    return False
-
-
-@when('I create a group "{name}"')
-def create_group_impl(context, name):
-    name = context.random_string(name)
-    context.group = context.api.create_group(name)
-
-
-@when('I try to create a group "{name}"')
-def try_create_group_impl(context, name):
-    name = context.random_string(name)
-    context.create_group_failed = False
-    try:
-        context.group = context.api.create_group(name)
-    except Exception as e:
-        context.create_group_failed = e
-
-
-@when('I add the user to the group')
-def add_user_to_group(context):
-    context.group.add_member(context.user)
-
-
-@when('I remove the user from the group')
-def remove_user_from_group(context):
-    context.group.remove_member(context.user)
-
-
-@then('The user is a member of the group')
-def user_is_a_member_of_group(context):
-    assert group_has_member(context.group, context.user)
-
-
-@then('the user is not a member of the group')
-def user_is_not_a_member_of_group(context):
-    assert not group_has_member(context.group, context.user)
-
-
-@then('it succeeds')
-def it_succeeds_impl(context):
-    if context.create_group_failed:
-        err = context.create_group_failed
-        context.create_group_failed = False
-        assert not err
-
-
-@then('I can list the group members')
-def list_members_impl(context):
+@when(u'I list members of "{kind}:{name}"')
+def list_members_impl(context, kind, name):
+    context.group = context.api.role(kind, name)
     context.group_members = context.group.members()
 
-
-@when('I add the member to the group')
-def add_member_impl(context):
-    context.group.add_member(context.user.role)
+@then(u'"{id}" is a member')
+def is_group_member(context, id):
+    print(context.group_members)
+    assert id in (x['member'] for x in context.group_members)
