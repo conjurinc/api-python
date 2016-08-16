b471f768eeb9447e8bbe1f0cef4dc3e137a29368
Rafał Rzepecki <divided.mind@gmail.com>
2016-08-16 19:05:09 +0200

    Pull by default on Jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@
 CONJUR_VERSION=${CONJUR_VERSION:-"latest"}
 DOCKER_IMAGE=${DOCKER_IMAGE:-"conjurinc/possum:$CONJUR_VERSION"}
 NOKILL=${NOKILL:-"1"}
-PULL=${PULL:-"0"}
+PULL=${PULL:-"1"}
 CMD_PREFIX=""
 
 function finish {
